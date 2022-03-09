4cf522b562d54c490755bb9de4c1533d313b7cd4
Firat Göktepe <firat@naxnordic.com>
2022-03-09 18:20:35 +0300

    fetched datas to UI

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
 
 const ProjectsPage = () => {
   return (
      <>
         <h1>Projects</h1>
-        <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
-     </> 
-    
+        <ProjectList projects={MOCK_PROJECTS}/>
+    </>
   )
 }
 
