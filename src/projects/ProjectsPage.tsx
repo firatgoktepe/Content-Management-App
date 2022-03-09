f253831571295d2d453b51ba44bc62fd447c4fea
Firat Göktepe <firat@naxnordic.com>
2022-03-09 17:36:23 +0300

    mocked projects data

---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { MOCK_PROJECTS } from './MockProjects';
+
+const ProjectsPage = () => {
+  return (
+     <>
+        <h1>Projects</h1>
+        <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+     </> 
+    
+  )
+}
+
+export default ProjectsPage
