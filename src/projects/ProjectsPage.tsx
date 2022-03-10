9cbfd2501057a78057d748cc135af667ae134262
Firat Göktepe <firat@naxnordic.com>
2022-03-10 16:39:06 +0300

    updated components to use API obj

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,52 @@
-import React, {useState} from 'react'
-import { MOCK_PROJECTS } from './MockProjects';
+import React, {useState, useEffect} from 'react'
+import { projectAPI } from './projectAPI'
 import ProjectList from './ProjectList';
 import {Project} from './Project'
 
 const ProjectsPage = () => {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS)
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string>("")
+
+  // Approach 1: using async await
+  useEffect( () => {
+    async function loadProjects() {
+      setLoading(true)
+      try {
+        const data = await projectAPI.get(1)
+        setError('')
+        setProjects(data)
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message)
+        }
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    loadProjects()
+  },[])
+
+
+  // Approach 2: using promise then
+//  useEffect(() => {
+//    setLoading(true);
+//    projectAPI
+//      .get(1)
+//      .then((data) => {
+//        setError(null);
+//        setLoading(false);
+//        setProjects(data);
+//      })
+//      .catch((e) => {
+//        setLoading(false);
+//        setError(e.message);
+//      });
+//  }, []);
+
+
+  
   const saveProject = (project: Project) => {
     let updateProjects = projects.map((p:Project) => {
       return p.id === project.id ? project : p
@@ -15,7 +57,29 @@ const ProjectsPage = () => {
   return (
      <>
         <h1>Projects</h1>
+
+        {error && (
+          <div className="row">
+            <div className="card large error">
+              <section>
+                <p>
+                  <span className="icon-alert inverse"></span>
+                  {error}
+                </p>
+              </section>
+            </div>
+          </div>
+        )} 
+
         <ProjectList projects={projects} onSave={saveProject} />
+
+        {loading && (
+          <div className="center-page">
+              <span className="spinner primary"></span>
+              <p>Loading...</p>
+          </div>
+        )}
+
     </>
   )
 }
--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -47,7 +47,6 @@ const delay =  (ms: number) => {
 const projectAPI = {
   get(page = 1, limit = 20){
     return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
-    .then(delay(600))
     .then(checkStatus)
     .then(parseJSON)
     .catch( (error: TypeError) => {
