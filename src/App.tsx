ded87bb49e2ed41f923173a47312fdab110824b0
Firat Göktepe <firat@naxnordic.com>
2022-03-10 17:49:02 +0300

    routing done

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,19 @@ import HomePage from './home/HomePage';
 function App() {
   return (
     <Router>
+      <header className="sticky">
+        <span className="logo"> 
+          <img src="assets/logo-3.svg" alt="logo" width="50" height="100" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          <span className="icon-calendar"></span>
+          Projects
+        </NavLink>
+      </header>
       <div className="container">
         <Routes>
           <Route path="/" element={<HomePage />} />
