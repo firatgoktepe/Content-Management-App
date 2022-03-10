4733dff4ac6034777d3586eeb08d784941f3d8b7
Firat Göktepe <firat@naxnordic.com>
2022-03-10 17:36:31 +0300

    created routing bw homepage and project page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import ProjectsPage from './projects/ProjectsPage';
+import HomePage from './home/HomePage';
 
 function App() {
   return (
-    <div className="container">
-      <ProjectsPage />
-    </div>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>  
   );
 }
 
