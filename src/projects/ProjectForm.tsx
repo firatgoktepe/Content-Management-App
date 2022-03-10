b0fbbfeb9e48790fb34da15c6cdaffdd6143f50a
Firat Göktepe <firat@naxnordic.com>
2022-03-10 12:52:42 +0300

    form state created

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, {SyntheticEvent, useState} from 'react'
+import {Project} from './Project'
 
 interface ProjectFormProps {
+    project: Project
     onCancel: () => void
+    onSave: (project: Project) => void
 }
 
-const ProjectForm = ( {onCancel}: ProjectFormProps ) => {
+const ProjectForm = ( { project: InitialProject, onCancel, onSave } : ProjectFormProps ) => {
+
+  const [project, setProject] = useState(InitialProject)
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault()
+    onSave(project)
+  }
+
+  const handleChange = (event: any) => {
+     const { type, name, value, checked } = event.target
+      let updatedValue = type === 'checkbox' ? checked : value
+      if (type === 'number'){
+        updatedValue = Number(updatedValue)
+      }
+      const change = {
+        [name]: updatedValue,
+      }
+
+      let updatedProject: Project  
+      setProject( (p) => {
+        updatedProject = new Project({...p, ...change})
+        return updatedProject
+      })
+  }
   return (
-  <form className="input-group vertical">
+  <form className="input-group vertical" onSubmit={handleSubmit}>
     <label htmlFor="name">Project Name</label>
-    <input type="text" name="name" placeholder="enter name" />
+    <input type="text" name="name" placeholder="enter name" value={project.name} onChange={handleChange} />
     <label htmlFor="description">Project Description</label>
 
-    <textarea name="description" placeholder="enter description"></textarea>
+    <textarea name="description" placeholder="enter description" value={project.description} onChange={handleChange}></textarea>
     <label htmlFor="budget">Project Budget</label>
 
-    <input type="number" name="budget" placeholder="enter budget" />
+    <input type="number" name="budget" placeholder="enter budget" value={project.budget} onChange={handleChange}/>
     <label htmlFor="isActive">Active?</label>
-    <input type="checkbox" name="isActive" />
+    <input type="checkbox" name="isActive" checked={project.isActive} onChange={handleChange}/>
 
     <div className="input-group">
         <button className="primary bordered medium">Save</button>
