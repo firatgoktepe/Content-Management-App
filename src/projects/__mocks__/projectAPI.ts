15ced07567e9b3e7bc460e764eefe047eac86609
Firat Göktepe <firat@naxnordic.com>
2022-03-14 14:54:35 +0300

    mocked API

---
+++ src/projects/__mocks__/projectAPI.ts
@@ -0,0 +1,9 @@
+import { MOCK_PROJECTS } from "../MockProjects";
+
+const projectAPI = {
+    get(page = 1, limit = 20) {
+        return Promise.resolve(MOCK_PROJECTS)
+    }
+}
+
+export { projectAPI }
